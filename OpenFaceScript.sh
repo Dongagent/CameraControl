bf3793d41c28e164571dc8d31aad4305f7e07832
Dongsheng Yang <yds343700931@gmail.com>
2021-05-24 18:34:45 +0900

    Write a script for batch processing multiple videos.

---
+++ OpenFaceScript.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Make Sure you are in the OpenFace/build path"
+workdir=`pwd`
+if ! [[ $workdir =~ OpenFace/build$ ]]; then
+    #statements
+    echo "Directory wrong"
+    exit
+fi
+
+read -p "Enter your video path/folder path:" input_path
+read -p "Enter your output path(default is ./temp_path_processed):" output_path
+output_path=${output_path:-temp_path_processed}
+files=`ls -d $input_path/*`
+for i in ${files[@]}; 
+do 
+    ./bin/FeatureExtraction -f $i -aus -out_dir $output_path
+done
